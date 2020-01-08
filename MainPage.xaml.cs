e3a4496f9ce2096f1f73f4a27c91cf79da158816
Seungkeun Lee <sngn.lee@samsung.com>
2020-01-08 15:29:07 +0900

    first

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Device.StartTimer(TimeSpan.FromMilliseconds(10), () =>
+            {
+                if (Progress.Progress == 1.0)
+                    Progress.Progress = 0;
+                Progress.Progress += 0.001;
+                return true;
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+[img](circleprogressbar.gif)
+
